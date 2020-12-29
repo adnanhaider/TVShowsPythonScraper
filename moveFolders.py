cb437389d2692a2f4eaeb3bc132f1169bdb02a1a
Syed Adnan Haider <adnanhaider530@gmail.com>
2020-12-29 14:32:34 +0500

    Update moveFolders.py

--- moveFolders.py
+++ moveFolders.py
@@ -2,15 +2,17 @@
 import os
 import shutil
 import re
+import datetime
+import time
 
 def MoveFolders():
-    os.chdir(os.path.join("C:", "\\Temp\\TV Shows"))
+    os.chdir(os.path.join("D:", "\\TV Shows"))
     dir_list = [name for name in os.listdir(".") if os.path.isdir(name)]
-    os.chdir(os.path.join("C:", "\\Temp\\New Downloads"))
+    os.chdir(os.path.join("D:", "\\New Downloads"))
     new_downloads_list = [name for name in os.listdir(".") if os.path.isdir(name)]
-    source = os.path.join("C:", "\\Temp\\New Downloads")
-    destination = os.path.join("C:", "\\Temp\\TV Shows")
-    
+    source = os.path.join("D:", "\\New Downloads")
+    destination = os.path.join("D:", "\\TV Shows")
+    current_time = str(datetime.datetime.now())+ '\t'
     for torrent in new_downloads_list:
         torrentFound = False
         for name in dir_list:
@@ -22,6 +24,7 @@ def MoveFolders():
                     if os.path.exists(os.path.join(full_destination,torrent)):
                         file = open("LOG 4 Already Existing Folders.txt", "a") 
                         file.write("\n") 
+                        file.write(current_time)
                         file.write(torrent)
                         file.write(' --> ')
                         file.write(full_destination)
@@ -30,6 +33,14 @@ def MoveFolders():
                     else:
                         print( f'Folder "{torrent}" has been moved to "{name}" directory inside TV Shows ')
                         shutil.move(os.path.join(source, torrent), full_destination)
+                        file = open("LOG 4 Moved Folders.txt", "a") 
+                        file.write("\n") 
+                        file.write(current_time)
+                        file.write(torrent)
+                        file.write(' --> ')
+                        file.write(full_destination)
+                        file.write('\n')
+                        file.close() 
                 except:
                     print('Something\'s up')
                 break
@@ -39,6 +50,7 @@ def MoveFolders():
         else:
             file = open("LOG 4 Not Found Folders.txt", "a")
             file.write("\n ") 
+            file.write(current_time)
             file.write(torrent)
             file.write('\n')
             file.close() 
@@ -48,7 +60,7 @@ def CheckExactMatch(torrent, name_parts):
     torrent_words = GetTorrentParts(torrent)
     parts_matched = []
     for part in name_parts:
-        for i, word in enumerate(torrent_words):
+        for word in torrent_words:
             if word == part.lower():
                 parts_matched.append(1)
                 break
@@ -59,7 +71,7 @@ def CheckExactMatch(torrent, name_parts):
 def GetTorrentParts(torrent_name):
     torrent_parts = []
     word_in_torrent = ''
-    is_last_word = False
+    # is_last_word = False
     tor_length = len(torrent_name)-1
     for i, char in enumerate(torrent_name):
         if ord(char) >= ord('a') and ord(char) <= ord('z') or ord(char) >= ord('A') and ord(char) <= ord('Z'):
@@ -75,9 +87,5 @@ def GetTorrentParts(torrent_name):
             word_in_torrent = ''
     return torrent_parts
 
-            
-
-
-        
-
-MoveFolders()
+if __name__ == "__main__":
+    MoveFolders()
